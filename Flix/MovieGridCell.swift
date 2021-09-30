eac094e6ffe76cba27cd9313a9a1e292b1114edb
ess-aar <sakr@seas.upenn.edu>
2021-09-30 14:13:09 -0400

    Movie Grid View

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Sakshi Rathore on 9/30/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
